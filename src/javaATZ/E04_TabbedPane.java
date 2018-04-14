bc4c9d2e48bbb9151fe560223664ca279368602d
milindoka <oak445@gmail.com>
2018-04-14 12:54:09 +0530

    Tabbed Pane Demo Complete

--- src/javaATZ/E04_TabbedPane.java
+++ src/javaATZ/E04_TabbedPane.java
@@ -3,7 +3,9 @@ package javaATZ;
 import javax.swing.*;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.event.*;
 
 
@@ -13,38 +15,32 @@ public class E04_TabbedPane extends JDialog
 
 public E04_TabbedPane() 
 {
-	
-    JTextArea ta=new JTextArea(200,200);  
     JPanel p1=new JPanel();  
-    p1.add(ta);  
+    p1.setPreferredSize(new Dimension(200, 200));
+    p1.setBackground(Color.RED);
+    
     JPanel p2=new JPanel();  
+    p2.setPreferredSize(new Dimension(200, 200));
+    p2.setBackground(Color.GREEN);
+    
     JPanel p3=new JPanel();  
+    p3.setPreferredSize(new Dimension(200, 200));
+    p3.setBackground(Color.BLUE);
+    
     JTabbedPane tp=new JTabbedPane();  
     tp.setBounds(50,50,200,200);  
     tp.add("Pane1",p1);  
     tp.add("Pane2",p2);  
     tp.add("Pane3",p3);    
 	
-setBounds(0,0 , 500, 275);
-setTitle("Tabbed Pane Demo");
-setLocationRelativeTo(null);
+    setBounds(0,0 , 500, 275);
+    setTitle("Tabbed Pane Demo");
+    setLocationRelativeTo(null);
 
-//JLabel labelWEST = new JLabel("WEST");
-//JLabel labelNORTH = new JLabel("NORTH",JLabel.CENTER);
-//JLabel labelCENTER = new JLabel("CENTER",JLabel.CENTER);
-//JLabel labelEAST = new JLabel("EAST");
+    Container dlgpane=getContentPane();
+    dlgpane.setLayout(new BorderLayout());
+    dlgpane.add(tp,BorderLayout.NORTH);
 
-
-Container dlgpane=getContentPane();
-dlgpane.setLayout(new BorderLayout());
-
-//dlgpane.add(labelWEST,BorderLayout.WEST);
-dlgpane.add(tp,BorderLayout.NORTH);
-//dlgpane.add(labelCENTER,BorderLayout.CENTER);
-//dlgpane.add(labelEAST,BorderLayout.EAST);
-//JPanel buttonpanel=new JPanel();
-
-
-}
+   }
 }
  
